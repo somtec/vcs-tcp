259145fdba1e073f2d4814afdd1f78f08e5b519a
Komisa72 <a.m.net@gmx.net>
2015-12-06 14:50:51 +0100

    added read_response function, is a stub.

--- bulletin_board_client/src/simple_message_client.c
+++ bulletin_board_client/src/simple_message_client.c
@@ -35,12 +35,17 @@
 /* macro used for printing source line etc. in verbose function */
 #define VERBOSE(...) verbose(__FILE__, __func__, __LINE__, __VA_ARGS__)
 
-/* Defines for the request entries */
+/* Defines for the request/response entries */
 #define SET_USER "user="
 #define SET_IMAGE "img="
+#define GET_STATUS "status="
+#define GET_FILE "file="
 
 /* Define for the request field terminator */
 #define FIELD_TERMINATOR '\n'
+
+/* Timeout for waiting on socket to become ready in seconds */
+#define SOCKET_TIMEOUT 10
 
 /*
  * ---------------------------------------------------------------- globals --
@@ -50,8 +55,8 @@
  * --------------------------------------------------------------- static --
  */
 
-/** Maximum path length of file system. */
-static long int smax_path = 0;
+/** Maximum filename length of file system. */
+static long int smax_filename = 0;
 
 /** Current program arguments. */
 static const char* sprogram_arg0 = NULL;
@@ -66,12 +71,13 @@ static void print_usage(FILE* file, const char* message, int exit_code);
 static void print_error(const char* message, ...);
 static int init(const char** program_args);
 static void cleanup(bool exit);
-static void verbose(const char* file_name, const char* function_name, int line, const char* message,
-        ...);
-static int execute(const char* server, const char* port, const char* user, const char* message,
-        const char* image_url);
-static int send_request(const char* user, const char* message, const char* image_url, int sfd);
-
+static void verbose(const char* file_name, const char* function_name, int line,
+    const char* message, ...);
+static int execute(const char* server, const char* port, const char* user,
+    const char* message, const char* image_url);
+static int send_request(const char* user, const char* message,
+    const char* image_url, int socket_fd);
+static int read_response(int socket_fd);
 
 /*
  * -------------------------------------------------------------- functions --
@@ -105,13 +111,13 @@ int main(int argc, const char* argv[])
 
     sprogram_arg0 = argv[0];
 
-    smc_parsecommandline(argc, argv, print_usage, &server, &port, &user, &message, &img_url,
-            &sverbose);
+    smc_parsecommandline(argc, argv, print_usage, &server, &port, &user,
+        &message, &img_url, &sverbose);
 
     errno = 0;
     port_nr = strtol(port, &end_ptr, 10);
     if ((errno == ERANGE && (port_nr == LONG_MAX || port_nr == LONG_MIN))
-            || (errno != 0 && port_nr == 0))
+        || (errno != 0 && port_nr == 0))
     {
         print_error("Can not convert port number (%s).", strerror(errno));
         return EXIT_FAILURE;
@@ -131,7 +137,7 @@ int main(int argc, const char* argv[])
 
     img_url_text = img_url == NULL ? "<no image>" : img_url;
     VERBOSE("Got parameter server %s, port %s, user %s, message %s, "
-            "image %s", server, port, user, message, img_url_text);
+        "image %s", server, port, user, message, img_url_text);
 
     /* so_REuseaddr mit setsockopt im server */
 
@@ -162,11 +168,11 @@ static int init(const char** program_args)
     sprogram_arg0 = program_args[0];
     VERBOSE("Initialize program.");
 
-    /* get maximum directory size */
-    smax_path = pathconf(".", _PC_PATH_MAX);
-    if (-1 == smax_path)
+    /* get maximum filename size */
+    smax_filename = pathconf(".", _PC_NAME_MAX);
+    if (-1 == smax_filename)
     {
-        smax_path = 0;
+        smax_filename = 0;
         print_error("pathconf() failed: %s.", strerror(errno));
         return EXIT_FAILURE;
     }
@@ -184,8 +190,8 @@ static int init(const char** program_args)
 static void cleanup(bool exit_program)
 {
 
-    fflush(stderr);
-    fflush(stdout);
+    (void) fflush(stderr); /* do not handle erros here */
+    (void) fflush(stdout);
 
     if (exit_program)
     {
@@ -210,11 +216,11 @@ static void print_error(const char* message, ...)
     va_list args;
 
     /* do not handle return value of fprintf, because it makes no sense here */
-    (void)fprintf(stderr, "%s: ", sprogram_arg0);
+    (void) fprintf(stderr, "%s: ", sprogram_arg0);
     va_start(args, message);
-    (void)vfprintf(stderr, message, args);
+    (void) vfprintf(stderr, message, args);
     va_end(args);
-    (void)fprintf(stderr, "\n");
+    (void) fprintf(stderr, "\n");
 }
 
 /**
@@ -238,14 +244,16 @@ static void print_usage(FILE* stream, const char* command, int exit_code)
     {
         print_error(strerror(errno));
     }
-    written = fprintf(stream, "  -s, --server <server>   full qualified domain "
-            "name or IP address of the server\n"
-            "  -p, --port <port>       well-known port of the server [0..65535]\n"
-            "  -u, --user <name>       name of the posting user\n"
-            "  -i, --image <URL>       URL pointing to an image of the posting user\n"
-            "  -m, --message <message> message to be added to the bulletin board\n"
-            "  -v, --verbose           verbose output\n"
-            "  -h, --help\n");
+    written =
+        fprintf(stream,
+            "  -s, --server <server>   full qualified domain "
+                "name or IP address of the server\n"
+                "  -p, --port <port>       well-known port of the server [0..65535]\n"
+                "  -u, --user <name>       name of the posting user\n"
+                "  -i, --image <URL>       URL pointing to an image of the posting user\n"
+                "  -m, --message <message> message to be added to the bulletin board\n"
+                "  -v, --verbose           verbose output\n"
+                "  -h, --help\n");
     if (written < 0)
     {
         print_error(strerror(errno));
@@ -270,15 +278,15 @@ static void print_usage(FILE* stream, const char* command, int exit_code)
  * \return void
  */
 static void verbose(const char* file_name, const char* function_name, int line,
-        const char* message, ...)
+    const char* message, ...)
 {
     int written;
     va_list args;
 
     if (sverbose > 0)
     {
-        written = fprintf(stdout, "%s [%s, %s(), line %d]: ", sprogram_arg0, file_name,
-                function_name, line);
+        written = fprintf(stdout, "%s [%s, %s(), line %d]: ", sprogram_arg0,
+            file_name, function_name, line);
         if (written < 0)
         {
             print_error(strerror(errno));
@@ -298,7 +306,6 @@ static void verbose(const char* file_name, const char* function_name, int line,
     }
 }
 
-
 /**
  * \brief Executes the request and get the response from server.
  *
@@ -311,35 +318,31 @@ static void verbose(const char* file_name, const char* function_name, int line,
  * /return EXIT_SUCCESS on success, else EXIT_FAILURE.
  */
 static int execute(const char* server, const char* port, const char* user,
-        const char* message, const char* image_url)
+    const char* message, const char* image_url)
 {
     struct addrinfo hints;
-    struct addrinfo* result;
-    struct addrinfo* rp;
-    int info;
-    int sfd;
-    ssize_t nread;
-    char buf[1000];
+    struct addrinfo* addr_result;
+    struct addrinfo* info;
+    int info_result;
+    int socket_fd;
     void* in_addr = NULL;
     char straddr[INET6_ADDRSTRLEN];
     struct sockaddr_in* s4;
     struct sockaddr_in6* s6;
-    fd_set set;
-    struct timeval timeout;
-    int ready;
+    int read_result;
+    int close_result;
 
     /* Obtain address(es) matching host/port */
-
     memset(&hints, 0, sizeof(struct addrinfo));
     hints.ai_family = AF_UNSPEC; /* Allow IPv4 or IPv6 */
     hints.ai_socktype = SOCK_STREAM; /* TCP socket */
     hints.ai_flags = 0;
-    hints.ai_protocol = IPPROTO_TCP; 
+    hints.ai_protocol = IPPROTO_TCP;
 
-    info = getaddrinfo(server, port, &hints, &result);
-    if (info != 0)
+    info_result = getaddrinfo(server, port, &hints, &addr_result);
+    if (info_result != 0)
     {
-        print_error("getaddrinfo: %s", gai_strerror(info));
+        print_error("getaddrinfo: %s", gai_strerror(info_result));
         return EXIT_FAILURE;
     }
 
@@ -348,90 +351,87 @@ static int execute(const char* server, const char* port, const char* user,
      If socket(2) (or connect(2)) fails, we (close the socket
      and) try the next address. */
 
-    for (rp = result; rp != NULL; rp = rp->ai_next)
+    for (info = addr_result; info != NULL; info = info->ai_next)
     {
-        sfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
-        if (sfd == -1)
+        socket_fd = socket(info->ai_family, info->ai_socktype,
+            info->ai_protocol);
+        if (socket_fd == -1)
             continue;
 
-        if (connect(sfd, rp->ai_addr, rp->ai_addrlen) != -1)
-            break; /* Success */
+        if (connect(socket_fd, info->ai_addr, info->ai_addrlen) != -1)
+            break; /* got a filedesriptor, success */
 
-        (void)close(sfd); /* error handling is not necessary, not interested */
-                          /* in this socket */
+        close_result = close(socket_fd);
+        if (close_result < 0)
+        {
+            print_error("Could not close tested socket: %s", strerror(errno));
+        }
     }
 
-    if (rp == NULL)
+    if (info == NULL)
     {
         /* No address succeeded */
         print_error("Could not connect %s:%s.", server, port);
-        freeaddrinfo(result);
+        freeaddrinfo(addr_result);
         return EXIT_FAILURE;
     }
 
     /* Determine IP family of found address */
-    switch (rp->ai_family)
+    switch (info->ai_family)
     {
-        case AF_INET:
-            /* IPv4 found */
-            s4 = (struct sockaddr_in *) rp->ai_addr;
-            in_addr = &s4->sin_addr;
-            break;
-        case AF_INET6:
-            /* IPv6 found */
-            s6 = (struct sockaddr_in6 *) rp->ai_addr;
-            in_addr = &s6->sin6_addr;
-            break;
-        default:
-            /* oh no, this should not happen */
-            print_error("Unknown address family: %d.", rp->ai_family);
-            freeaddrinfo(result);
-            return EXIT_FAILURE;
+    case AF_INET:
+        /* IPv4 found */
+        s4 = (struct sockaddr_in *) info->ai_addr;
+        in_addr = &s4->sin_addr;
+        break;
+    case AF_INET6:
+        /* IPv6 found */
+        s6 = (struct sockaddr_in6 *) info->ai_addr;
+        in_addr = &s6->sin6_addr;
+        break;
+    default:
+        /* oh no, this should not happen */
+        print_error("Unknown address family: %d.", info->ai_family);
+        freeaddrinfo(addr_result);
+        close_result = close(socket_fd);
+        if (close_result < 0)
+        {
+            print_error("Could not close socket: %s", strerror(errno));
+        }
+        return EXIT_FAILURE;
 
     }
     /* Convert IP address into printable format */
-    if (NULL == inet_ntop(rp->ai_family, in_addr, straddr, sizeof(straddr)))
+    if (NULL == inet_ntop(info->ai_family, in_addr, straddr, sizeof(straddr)))
     {
         print_error("Could not convert server address: %s.", strerror(errno));
     }
     else
     {
-        VERBOSE("Connection to %s (%s) on port %s established!", server, straddr,
-                port);
+        VERBOSE("Connection to %s (%s) on port %s established!", server,
+            straddr, port);
     }
 
-    freeaddrinfo(result); /* No longer needed */
+    freeaddrinfo(addr_result); /* No longer needed */
 
-    if (send_request(user, message, image_url, sfd) != EXIT_SUCCESS)
+    if (send_request(user, message, image_url, socket_fd) != EXIT_SUCCESS)
     {
+        close_result = close(socket_fd);
+        if (close_result < 0)
+        {
+            print_error("Could not close socket: %s", strerror(errno));
+        }
         return EXIT_FAILURE;
     }
 
-    /* Initialize the file descriptor set. */
-    FD_ZERO(&set);
-    FD_SET(sfd, &set);
-
-    /* Initialize the timeout data structure. */
-    timeout.tv_sec = 10;
-    timeout.tv_usec = 0;
-
-    /* select returns 0 if timeout, 1 if input available, -1 if error. */
-    ready = select(sfd + 1, &set, NULL, NULL, &timeout);
-    if (ready < 0)
+    read_result = read_response(socket_fd);
+    close_result = close(socket_fd);
+    if (close_result < 0)
     {
-        /* can not handle errors or signals */
-        print_error(strerror(errno));
+        print_error("Could not close socket: %s", strerror(errno));
     }
+    return read_result;
 
-    nread = read(sfd, buf, 1000);
-    if (nread == -1)
-    {
-        perror("read");
-        exit(EXIT_FAILURE);
-    }
-    VERBOSE("Received %ld bytes.", (long ) nread);
-
-    return EXIT_SUCCESS;
 }
 
 /**
@@ -440,12 +440,12 @@ static int execute(const char* server, const char* port, const char* user,
  * /param user which wrote the message.
  * /param message to be shown in bulletin board.
  * /param image_url URL of image or NULL.
- * /param sfd open socket file descriptor.
+ * /param socket_fd open socket file descriptor.
  *
- * /return EXIT_SUCCESS on succuss, else EXIT_FAILURE.
+ * /return EXIT_SUCCESS on success, else EXIT_FAILURE.
  */
 static int send_request(const char* user, const char* message,
-        const char* image_url, int sfd)
+    const char* image_url, int socket_fd)
 {
     size_t len_user;
     size_t len_user_data;
@@ -496,13 +496,13 @@ static int send_request(const char* user, const char* message,
     }
     strncpy(destination, message, len_message);
     /** @TODO error handling */
-    if (write(sfd, send_buf, len) != (ssize_t) len)
+    if (write(socket_fd, send_buf, len) != (ssize_t) len)
     {
         print_error("partial/failed write.");
         free(send_buf);
         return EXIT_FAILURE;
     }
-    if (0 != shutdown(sfd, SHUT_WR)) /* no more writes */
+    if (0 != shutdown(socket_fd, SHUT_WR)) /* no more writes */
     {
         print_error("Could not shutdown write connection: %s", strerror(errno));
         free(send_buf);
@@ -510,4 +510,142 @@ static int send_request(const char* user, const char* message,
     }
     free(send_buf);
     return EXIT_SUCCESS;
+}
+
+/**
+ * /brief Read response from server.
+ *
+ * /param socket_fd open socket file descriptor.
+ *
+ * /return EXIT_SUCCESS on success, else error status from server.
+ */
+static int read_response(int socket_fd)
+{
+    ssize_t read_count;
+    fd_set set;
+    struct timeval timeout;
+    int ready;
+    char* read_buf;
+    char* parse_buf;
+    bool finished = false;
+    int result;
+    size_t read_size;
+    bool expect_status = true;
+    //bool expect_file = false;
+    char* current_write_pos;
+    size_t amount;
+    //size_t parse_amount;
+    size_t len_status;
+    //size_t len_file;
+    // int number_of_files = 0;
+
+    len_status = strlen(GET_STATUS);
+    //len_file = strlen(GET_FILE);
+
+    read_size = smax_filename + 1;
+
+    /* filenames must not exceed maximum name length of system */
+    read_buf = malloc(read_size * sizeof(char));
+    if (read_buf == NULL)
+    {
+        print_error("Can not allocate read buffer: %s.", strerror(ENOMEM));
+        return EXIT_FAILURE;
+    }
+    parse_buf = malloc(2 * read_size * sizeof(char));
+    if (parse_buf == NULL)
+    {
+        print_error("Can not allocate parse buffer: %s.", strerror(ENOMEM));
+        free(read_buf);
+        print_error(strerror(ENOMEM));
+        return EXIT_FAILURE;
+    }
+
+    /* Initialize the file descriptor set. */
+    FD_ZERO(&set);
+    FD_SET(socket_fd, &set);
+
+    result = EXIT_FAILURE;
+    current_write_pos = parse_buf;
+    amount = 0;
+    //parse_amount = 0;
+
+    while (!finished)
+    {
+        /* Initialize the timeout data structure. */
+        timeout.tv_sec = SOCKET_TIMEOUT;
+        timeout.tv_usec = 0;
+
+        /* select returns 0 if timeout, 1 if input available, -1 if error. */
+        ready = select(socket_fd + 1, &set, NULL, NULL, &timeout);
+        if (ready < 0)
+        {
+            /* can not handle errors or signals */
+            print_error(strerror(errno));
+            finished = true;
+            continue;
+        }
+        if (ready == 0)
+        {
+            /* timeout */
+            print_error("Timeout on receiving response.");
+            finished = true;
+            continue;
+        }
+
+        read_count = read(socket_fd, read_buf, read_size);
+        amount += read_count;
+        if (read_count == -1)
+        {
+            print_error("read failed: %s", strerror(errno));
+            free(read_buf);
+            free(parse_buf);
+            return EXIT_FAILURE;
+        }
+        if (read_count == 0)
+        {
+            finished = true;
+            /* end of file */
+        }
+        VERBOSE("Received %ld bytes.", (long ) read_count);
+
+        if (expect_status)
+        {
+            if (read_count > 0)
+            {
+                memcpy(current_write_pos + read_count, read_buf, read_count);
+                current_write_pos += read_count;
+            }
+#if 0
+            if (finished)
+            {
+                expect_file = true;
+            }
+#endif
+            if (finished || (amount >= read_size))
+            {
+                /* it must contain the status= string now */
+
+                if (amount < (len_status + 2))
+                {
+                    /* message is too short */
+                    finished = true;
+                    print_error("Malformed response.");
+                    continue;
+                }
+                if (strncmp(GET_STATUS, parse_buf, len_status) != 0)
+                {
+                    finished = true;
+                    print_error("Malformed response.");
+                    continue;
+                }
+
+
+            }
+        }
+    }
+    free(read_buf);
+    free(parse_buf);
+
+    return result;
+
 }
