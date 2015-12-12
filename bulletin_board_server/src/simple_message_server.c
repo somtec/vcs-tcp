481701e43a58d034d0ff40e6207d130a31c2f049
Komisa72 <a.m.net@gmx.net>
2015-12-13 00:39:03 +0100

    param_check: improved reading/checking of parameters. setup_connection: only IPV4 addresses are bound now.

--- bulletin_board_server/src/simple_message_server.c
+++ bulletin_board_server/src/simple_message_server.c
@@ -7,8 +7,7 @@
  *
  * @author Andrea Maierhofer    1410258024  <andrea.maierhofer@technikum-wien.at>
  * @author Thomas Schmid        1410258013  <thomas.schmid@technikum-wien.at>
- * @date 2015/12/07
- *
+ * @date 2015/12/12
  *
  */
 
@@ -29,6 +28,7 @@
 #include <stdbool.h>
 #include <limits.h>
 #include <stdarg.h>
+#include <getopt.h>
 
 /*
  * ---------------------------------------------------------------- defines --
@@ -41,7 +41,7 @@
 #define BUSINESS_LOGIC_PATH "/usr/local/bin/simple_message_server_logic"
 
 #define LOWER_PORT_RANGE 0
-#define UPPER_PORT_RANGE 65536
+#define UPPER_PORT_RANGE 65535
 /* handle up to max connections */
 #define MAX_CONNECTION 15
 
@@ -57,14 +57,13 @@ static const char* sprogram_arg0 = NULL;
 /*
  * ------------------------------------------------------------- prototypes --
  */
-static void param_check(int argc, char** argv, char** port_nr);
-static void kill_child_zombies(int signal);
-static int reg_sig_handler(void);
-static int do_connection(int socket_fd);
 static void print_error(const char* message, ...);
 static void print_usage(FILE* file, const char* message, int exit_code);
-static int set_up_connection(const char* port_nr);
-
+static void param_check(int argc, const char* const argv[], uint16_t* port_nr);
+static int register_signal_handler(void);
+static void kill_child_handler(int signal);
+static int setup_connection(uint16_t port_nr);
+static int do_connection(int socket_fd);
 /*
  * -------------------------------------------------------------- functions --
  */
@@ -80,27 +79,24 @@ static int set_up_connection(const char* port_nr);
  * \retval EXIT_FAILURE on failure.
  *
  */
-int main(int argc, char** argv)
+int main(int argc, const char* const argv[])
 {
     /* server port with type short int which is needed by the htons function */
-    char* server_port = NULL;
+    uint16_t server_port;
     int socket_fd;
 
     sprogram_arg0 = argv[0];  /* must contain the filename anyway */
 
-    /* calling the getopt function to get portnum*/
+    /* calling the getopt function to get server_port*/
     param_check(argc, argv, &server_port);
-    if (server_port == NULL)
-    {
-        return EXIT_FAILURE;
-    }
 
-    if ((socket_fd = set_up_connection(server_port)) < 0)
+    if ((socket_fd = setup_connection(server_port)) < 0)
     {
         return EXIT_FAILURE;
     }
-    if (reg_sig_handler() < 0)
+    if (register_signal_handler() < 0)
     {
+        (void) close(socket_fd);
         return EXIT_FAILURE;
     }
     if (do_connection(socket_fd) < 0)
@@ -108,9 +104,8 @@ int main(int argc, char** argv)
         return EXIT_FAILURE;
     }
 
-    /* should never come here */
+    /* should never come here, assert due to our c rules */
     assert(0);
-
     return EXIT_SUCCESS;
 }
 
@@ -170,63 +165,69 @@ static void print_usage(FILE* stream, const char* command, int exit_code)
 
     exit(exit_code);
 }
-/**
- * \brief Cleanup the program.
- *
- * \param exit_program when set to TRUE exit program immediately with EXIT_FAILURE.
- *
- * \return void
- */
 
 /**
- * \brief the method for checking the passed parameters
+ * \brief Method for checking the passed parameters.
  *
  * This functions calls exit indirectly when passed parameters are invalid.
  *
- * \param argc the number of arguments
- * \param argv the arguments itselves (including the program name in argv[0])
- * \param port_str resulting port string for further usage
+ * \param argc the number of arguments.
+ * \param argv the arguments itself (including the program name in argv[0]).
+ * \param port_nr resulting port number for further usage.
  *
  */
-static void param_check(int argc, char **argv, char** port_str)
+static void param_check(int argc, const char* const argv[], uint16_t* port_nr)
 {
     char* end_ptr;
-    long int port_nr = 0;
+    long int port_nr_convert;
     int c;
+    const char* port = NULL;
+
+    struct option long_options[] =
+    {
+        {"port", 1, NULL, 'p'},
+        {"help", 0, NULL, 'h'},
+        {0, 0, 0, 0}
+    };
 
     opterr = 0;
-
-    *port_str = NULL;
-    if (argc < 3)
+    if (argc < 2)
     {
-        print_usage(stderr, sprogram_arg0, EXIT_FAILURE);
+        print_usage(stderr, argv[0], EXIT_FAILURE);
     }
 
-    while ((c = getopt(argc, argv, "p:h::")) != EOF)
+    while ((c = getopt_long(argc, (char** const) argv, "p:h", long_options,
+            NULL)) != EOF)
     {
         switch (c)
         {
         case 'p':
-            port_nr = strtol(optarg, &end_ptr, INPUT_NUM_BASE);
-            if ((errno == ERANGE && (port_nr == LONG_MAX || port_nr == LONG_MIN))
-                    || (errno != 0 && port_nr == 0))
+            port = optarg;
+            if (optarg != NULL)
             {
-                print_error("Can not convert port number (%s).", strerror(errno));
-                print_usage(stderr, sprogram_arg0, EXIT_FAILURE);
-            }
+                port_nr_convert = strtol(optarg, &end_ptr, INPUT_NUM_BASE);
+                if ((errno == ERANGE &&
+                        (port_nr_convert == LONG_MAX || port_nr_convert == LONG_MIN))
+                    || (errno != 0 && port_nr_convert == 0))
+                {
+                    print_error("Can not convert port number (%s).", strerror(errno));
+                    print_usage(stderr, sprogram_arg0, EXIT_FAILURE);
+                }
 
-            if (end_ptr == optarg)
-            {
-                print_error("No digits were found.");
-                print_usage(stderr, sprogram_arg0, EXIT_FAILURE);
-            }
+                if (end_ptr == optarg)
+                {
+                    print_error("No digits were found.");
+                    print_usage(stderr, sprogram_arg0, EXIT_FAILURE);
+                }
 
-            if (port_nr < LOWER_PORT_RANGE || port_nr > UPPER_PORT_RANGE)
-            {
-                print_error("Port number out of range.");
-                print_usage(stderr, sprogram_arg0, EXIT_FAILURE);
+                if (port_nr_convert < LOWER_PORT_RANGE || port_nr_convert > UPPER_PORT_RANGE)
+                {
+                    print_error("Port number out of range.");
+                    print_usage(stderr, sprogram_arg0, EXIT_FAILURE);
+                }
+                /* set resulting port number */
+                *port_nr = (uint16_t) port_nr_convert;
             }
-            *port_str = optarg;
             break;
         case 'h':
             /* when the usage message is requested, program will exit afterwards */
@@ -241,7 +242,7 @@ static void param_check(int argc, char **argv, char** port_str)
     }
 
     /* if user added extra arguments */
-    if (argc - optind >= 1)
+    if ((optind != argc) || (port == NULL))
     {
         print_usage(stderr, sprogram_arg0, EXIT_FAILURE);
     }
@@ -249,31 +250,33 @@ static void param_check(int argc, char **argv, char** port_str)
 }
 
 /**
- * \brief Install signal handler for killing child zombie processes.
+ * \brief Install signal handler for waiting on child processes.
  *
  * If the functions fails then errno is set to EINVAL, see sigaction.
  *
  * \return 0 if sighandler was installed, else -1 on failure.
  */
-int reg_sig_handler(void)
+int register_signal_handler(void)
 {
     struct sigaction sig;
-    sig.sa_handler = kill_child_zombies;
+    sig.sa_handler = kill_child_handler;
     /*
      * excludes all signals from the signal handler mask.
      * Return value can be ignored, is always 0.
      */
     (void) sigemptyset(&sig.sa_mask);
-    sig.sa_flags = SA_RESTART;
+    sig.sa_flags = SA_RESTART; /* see beej, Restart syscall on signal return. */
 
     errno = 0;
     return sigaction(SIGCHLD, &sig, NULL);
 }
 
 /**
- * \brief Kill all my children otherwise they will be zombies.
+ * \brief Wait for all my children to be killed otherwise they will be zombies.
+ *
+ * \param signal of child will be ignored.
  */
-static void kill_child_zombies(int signal)
+static void kill_child_handler(int signal)
 {
     /*
      * waitpid waits for information about child-processes
@@ -281,120 +284,104 @@ static void kill_child_zombies(int signal)
      * not interested in status of child process.
      * WNOHANG makes this function non-blocking
      */
-    (void) signal;
+    (void) signal; /* pedantic */
     while (waitpid((pid_t) (WAIT_ANY), NULL, WNOHANG) > 0)
     {
     }
 }
 
-static int set_up_connection(const char* port_nr)
+/**
+ * \brief Setup the connection for a tcp socket.
+ *
+ * \param port_nr where this server listens.
+ * \return On success a valid socket descriptor or -1 in case of failure.
+ */
+static int setup_connection(uint16_t port_nr)
 {
-    int socket_fd = 0;
-    struct addrinfo hints;
-    struct addrinfo* result;
-    struct addrinfo* rp;
+    int socket_fd;
+    /* set SO_REUSEADDR on a socket to true (1) */
+    int optval = 1;
+    struct sockaddr_in serveraddr;
 
-    /* resetting the structs */
-    memset(&hints, 0, sizeof(hints));
-
-    hints.ai_family = AF_UNSPEC; /* Allow IPv4 or IPv6 */
-    hints.ai_socktype = SOCK_STREAM; /* Datagram socket */
-    hints.ai_flags = AI_PASSIVE; /* For my IP */
-
-    /*
-     * ### FB_CF:  Also hier getaddrinfo() zu verwenden bringt's nicht so
-     *             wirklich. - Außerdem wissen Sie dadurch nicht, ob Ihr Server
-     *             nun auf einer IPV4 oder einer IPV6 Adresse lauscht ...
-     */
-
-    /*
-     * getting a linked list of available adresses.
-     * stores the first one in &result
-     */
-    if (getaddrinfo(NULL, port_nr, &hints, &result) != 0)
+    /* support only IP V4 */
+    if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
-
-        print_error("error getaddrinfo\n");
-        return -1;
+       print_error("socket() IPV4 failed.");
+       return -1;
     }
 
-    /*
-     * trys to bind to the available adresses.
-     * breaks, if binding to one was successful.
-     */
-    for (rp = result; rp != NULL; rp = rp->ai_next)
+    if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, (char*)&optval,
+            sizeof(optval)) < 0)
     {
-        socket_fd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
-        if (socket_fd == -1)
-        {
-            continue;
-        }
-        if (bind(socket_fd, rp->ai_addr, rp->ai_addrlen) == 0)
-        {
-            break;
-        }
-        close(socket_fd);
+       print_error("setsockopt(SO_REUSEADDR) failed.");
+       (void) close(socket_fd);
+       return -1;
     }
 
-    /* no address succeeded */
-    if (rp == NULL)
-    {
-        freeaddrinfo(result);
-        print_error("error bind\n");
-        return -1;
-    }
+    memset(&serveraddr, 0, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    /* port number must be converted to network byte order */
+    serveraddr.sin_port   = htons(port_nr);
+    /* IPV 4 address to accept any incoming messages */
+    serveraddr.sin_addr.s_addr   = htonl(INADDR_ANY);
 
-    freeaddrinfo(result);
+    if (bind(socket_fd, (struct sockaddr*)&serveraddr, sizeof(serveraddr)) < 0)
+    {
+         print_error("bind() failed.");
+         (void) close(socket_fd);
+         return -1;
+      }
 
     if (listen(socket_fd, MAX_CONNECTION) < 0)
     {
-        print_error("error listen\n");
+        print_error("listen() failed.");
         (void) close(socket_fd);
-        return EXIT_FAILURE;
+        return -1;
     }
-
     return socket_fd;
 }
 
 /**
  * \brief handle the connections of socket_fd.
+ *
+ * This function serves client request in a loop, so it should never exit.
+ *
+ * \param socket_fd listening socket.
+ * \return -1 in case of a 'weird' program execution.
  */
 static int do_connection(int socket_fd)
 {
     int pid;
     struct sockaddr_storage addr_inf;
     socklen_t socklen = sizeof(addr_inf);
-    int connection_fd = 0;
+    int connection_fd;
+    int written;
 
     while (1)
     {
         if ((connection_fd = accept(socket_fd, (struct sockaddr*) &addr_inf, &socklen)) < 0)
         {
-            print_error("Accept failed.\n");
+            print_error("accept() failed.\n");
             continue;
         }
 
-        (void) fprintf(stdout, "accepting connection worked\n");
-        (void) fprintf(stdout, "listening socket: %d\n", socket_fd);
-        (void) fprintf(stdout, "connected socket: %d\n", connection_fd);
-
         if ((pid = fork()) < 0)
         {
-            print_error("error on forking\n");
+            print_error("fork() failed.");
             (void) close(socket_fd);
             (void) close(connection_fd);
-            return EXIT_FAILURE;
+            return -1;
         }
         /* code, executed by the child process */
         if (pid == 0)
         {
-            (void) fprintf(stdout, "forking worked\n");
-            /* child process doesn't need listening socket */
-            int debug = 1;
-            while (debug == 1)
+            written = fprintf(stdout, "fork() successful.");
+            if (written < 0)
             {
-                debug = 1;
+                print_error(strerror(errno));
             }
+
+            /* child process doesn't need listening socket */
             if (close(socket_fd) != 0)
             {
                 print_error("Child process could not close listening socket.");
@@ -425,20 +412,18 @@ static int do_connection(int socket_fd)
             if (execl(BUSINESS_LOGIC_PATH, BUSINESS_LOGIC, NULL) < 0)
             {
                 print_error("Could not start server business logic.\n");
-                (void) close(connection_fd);
-                return EXIT_FAILURE;
+                exit(EXIT_FAILURE);
             }
-            (void) close(connection_fd);
-            return EXIT_SUCCESS;
-
+            assert(0);  /*never come here after successful execl */
+            exit(EXIT_FAILURE);
         }
         else
         {
             /* code, executed by the server process */
             /* in the parent */
             /*
-             * if an error occurs when trying to close the listening socket in the parent process,
-             * it can be ignored
+             * if an error occurs when trying to close the listening socket in
+             * the parent process, it can be ignored
              */
             (void) close(connection_fd);
         }
